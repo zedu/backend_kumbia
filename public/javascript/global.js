f8928e25ed19e72947b4c527808a977138f88c18
Manuel Aguirre <programador.manuel@gmail.com>
2012-01-12 15:12:04 -0430

    edicion de scripts

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -2,6 +2,6 @@ $(function(){
     $(':button, :submit, .actions a').addClass('btn');
     $('.alert-message .close').click(function(event){
         event.preventDefault();
-        $(this).parent().hide(400);
+        $(this).parent().fadeOut(400);
     });
 });
