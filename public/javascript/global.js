5566a0bef9165138bc19b07a763797027d32f88d
Manuel Aguirre <programador.manuel@gmail.com>
2012-01-12 16:43:43 -0430

    cambios en los script

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -1,5 +1,4 @@
 $(function(){
-    $(':button, :submit, .actions a').addClass('btn');
     $('.alert-message .close').click(function(event){
         event.preventDefault();
         $(this).parent().fadeOut(400);
